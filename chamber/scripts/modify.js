d277be0ef866142b9e823f1fed0c8866f54f38e1
CastellaniJuan <castellanijuanignacio@gmail.com>
2022-10-08 19:37:55 -0300

    chaber

--- chamber/scripts/modify.js
+++ chamber/scripts/modify.js
@@ -1,9 +1,19 @@
 let LastModify = new Date(document.lastModified);
 let currentDate= new Date();
 let year = currentDate.getFullYear();
+
+
+let month = new Intl.DateTimeFormat('en', { month: 'long' }).format(currentDate);
+let day = new Intl.DateTimeFormat('en', { weekday: 'long' }).format(currentDate);
+let date = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(currentDate);
+
+
+
 let hours = LastModify.getHours();
 let minutes = LastModify.getMinutes();
 let seconds = LastModify.getSeconds();
 
+
+document.getElementById("date").textContent = day + ", " + date + " " + month + " " + year;
 document.getElementById("modify").textContent += LastModify.toLocaleDateString("en-US") +" "+hours+":"+minutes+":"+seconds;
 document.getElementById("copyright").textContent = year;
--- lesson3/scripts/modify.js
+++ lesson3/scripts/modify.js
@@ -1,9 +1,9 @@
 let LastModify = new Date(document.lastModified);
-let currentDate= new Date()
-let year = currentDate.getFullYear()
-let hours = LastModify.getHours()
-let minutes = LastModify.getMinutes()
-let seconds = LastModify.getSeconds()
+let currentDate= new Date();
+let year = currentDate.getFullYear();
+let hours = LastModify.getHours();
+let minutes = LastModify.getMinutes();
+let seconds = LastModify.getSeconds();
 
 document.getElementById("modify").textContent += LastModify.toLocaleDateString("en-US") +" "+hours+":"+minutes+":"+seconds;
 document.getElementById("copyright").textContent = year;
