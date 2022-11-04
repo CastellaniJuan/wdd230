e05a2a662dba3eb7c83c3303eb5c0b1c3fab8a87
CastellaniJuan <castellanijuanignacio@gmail.com>
2022-11-03 23:38:09 -0300

    Update localstorage-days.js

--- chamber/scripts/localstorage-days.js
+++ chamber/scripts/localstorage-days.js
@@ -3,7 +3,7 @@ const displayVisit = document.getElementById("lsvisit");
 let LastVisit = Number(window.localStorage.getItem("lsvisit"));
 
 let today = Math.round(currentDate/(1000 * 3600 * 24))
-const dayDiff = 0;
+let dayDiff = 0;
 
 if (LastVisit == 0){
     
@@ -11,8 +11,8 @@ displayVisit.textContent = "This is your first day visiting us!";
 }else{
     dayDiff = today - LastVisit;
     LastVisit = today;
-    displayVisit.textContent = "Your last visit was " + dayDiff + " ago";
+    displayVisit.textContent = "Your last visit was " + dayDiff + " days ago";
 
 }
-
+window.localStorage.setItem("lsvisit",today);
 
