494502435153453c4feecd0647bd14e991dc0a77
CastellaniJuan <castellanijuanignacio@gmail.com>
2022-11-19 00:18:03 -0300

    weather chamber

---
+++ chamber/scripts/weather-api.js
@@ -0,0 +1,37 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#temp');
+const weatherIcon = document.querySelector('#weatherimg');
+const captionDesc = document.querySelector('#climate');
+const windSpeed = document.querySelector('#windspeed');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-34.82&lon=-58.46&appid=e9b006d203c58c92685fd6d9f9321fb0&units=metric';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc.charAt(0).toUpperCase() + desc.slice(1);
+    windSpeed.textContent = weatherData.wind.speed;
+  }
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -6,8 +6,8 @@ const windchill = document.getElementById("windchill");
 var chillvalue = "";
 if (temp <= 10 && windspeed >= 4.8) {
    chillvalue = (13.12 + 0.6215*temp - 11.37 * (windspeed**0.16) + 0.3965 * temp * (windspeed**0.16)).toFixed(1);
-
+   windchill.textContent = chillvalue + "ºC";
 }else{
     chillvalue = "N/A";
+    windchill.textContent = chillvalue
 }
-windchill.textContent = chillvalue + "ºC";
