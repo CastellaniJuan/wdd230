1196c9033ef540c45da8242a3765e016dfa2af55
CastellaniJuan <castellanijuanignacio@gmail.com>
2022-12-15 05:24:53 -0300

    update

--- final-project/scripts/freshscript.js
+++ final-project/scripts/freshscript.js
@@ -1,5 +1,17 @@
-const counterdisplay = document.getElementById("drinksmade-text");
+const url = '../data/fruits.json';
 
-let counter = Number(window.localStorage.getItem("freshcounter"));
-
-counterdisplay.innerHTML = counter;
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      // displayResults(data);
+      //displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
