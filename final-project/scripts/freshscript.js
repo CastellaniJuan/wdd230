a3782a46c594a19339e65c840a28062eeb1aef8a
CastellaniJuan <castellanijuanignacio@gmail.com>
2022-12-15 05:28:56 -0300

    Update freshscript.js

--- final-project/scripts/freshscript.js
+++ final-project/scripts/freshscript.js
@@ -1,17 +1,12 @@
-const url = '../data/fruits.json';
+const requestURL = 'https://raw.githubusercontent.com/CastellaniJuan/wdd230/main/final-project/data/fruits.json';
 
-async function apiFetch() {
-  try {
-    const response = await fetch(url);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data); // this is for testing the call
-      // displayResults(data);
-      //displayResults(data);
-    } else {
-      throw Error(await response.text());
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
+
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        //const companies = jsonObject['companies'];
+        //companies.forEach(displayCompanies);
+    });
